2bd4241ae6dd2260da77fb168b5f7c5ff138060c
tranvanthe10101 <tranvanthe10101@gmail.com>
2022-06-12 19:44:40 +0700

    add

---
+++ Slot12/Slot12/DemoFileInfo/FileInfo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Text;
+namespace Demo_FileInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string FileName = @"MyFile.txt";
+            Console.WriteLine(" *** Demo FileInfo ***\n");
+            File.WriteAllText(FileName, "Hello world");
+            Console.WriteLine("Read file: ");
+            string content = File.ReadAllText(FileName);
+            Console.WriteLine(content);
+            Console.WriteLine("File info: ");
+            FileInfo testFile = new FileInfo(FileName);
+            Console.WriteLine($"Name:{testFile.Name}");
+            Console.WriteLine($"Create time: {testFile.CreationTime}");
+            Console.WriteLine($"Last Wite time: {testFile.LastWriteTime}");
+            Console.WriteLine($"Directory Name: {testFile.DirectoryName}");
+            Console.ReadLine();
+        }
+    }
+}
